6c5c9b1be6189dc84e2bbc227af7f49793311cf0
candy <1192979260@qq.com>
2016-04-05 10:45:17 +0800

    es6 babel

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var express=require('express');
-var app=express();
-var bodyParser = require('body-parser');//使用body-parser进行post参数的解析,可获得一个JSON化的req.body
-var route=require('./route');
-var config=require('./config');
-var cookieParser=require('cookie-parser');
-var session=require('express-session');
+// var express=require('express');
+import express from 'express';
+const app=express();
+import bodyParser from 'body-parser';//使用body-parser进行post参数的解析,可获得一个JSON化的req.body
+import route from'./route';
+import config from'./config';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 
 app.set('views','./views');
@@ -17,24 +18,24 @@ app.use(session({
   cookie:{maxAge:30000}
 }));
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.locals.user = req.session.user;
-  var err = req.session.error;
-  var success = req.session.success;
+  let err = req.session.error;
+  let success = req.session.success;
   delete req.session.error;
   delete req.session.success;
   res.locals.message = '';
   if (err)
-    res.locals.message = '<div class="alert alert-warning">    ' + err + '</div>';
+    res.locals.message = '<div class="alert alert-warning">' + err + '</div>';
   else if (success)
-    res.locals.message = '<div class="alert alert-success">    ' + success + '</div>';
+    res.locals.message = '<div class="alert alert-success">' + success + '</div>';
   next();
 });
 
 
-var server = app.listen(config.server.port,function(){
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(config.server.port,() => {
+  let host = server.address().address;
+  let port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
 });
 
---
+++ babel.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Willin on 2016-4-5.
+ */
+require('babel-register');
+module.exports=require('./app');
